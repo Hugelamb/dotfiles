f11520db38c12c99beda4e886a1cdbe93dda8796
Hugh Lam <hughrjlam@gmail.com>
2025-03-02 22:26:06 +1100

    Added lots more useful luasnippet utility functions and snippets for latex notetaking

--- .config/nvim/luasnippets/tex/fonts.lua
+++ .config/nvim/luasnippets/tex/fonts.lua
@@ -8,7 +8,7 @@ return {
       ]],
       {i(1)}
     ),
-    { condition = in_text }
+    { condition = tex_utils.in_text }
   ),
   s({ trig = ";it", dscr = "italic typeface", snippetType="autosnippet" },
     fmta(
@@ -17,6 +17,26 @@ return {
       ]],
       { i(1) }
     ),
-    { condition = in_text }
+    { condition = tex_utils.in_text }
   ),
+  ms({      
+    common = { dscr = "Enter \\textcolor{}{} command", snippetType = "autosnippet", condition = tex_utils.in_mathzone },
+    "colour",
+    "color",
+    { trig = "colour", snippetType = "snippet", condition = tex_utils.in_text },
+    { trig = "color", snippetType = "snippet", condition = tex_utils.in_text },
+    ";clr",
+    { trig = ";clr", condition = tex_utils.in_text },
+  }, {
+      -- Snippet contents?
+      t"\\textcolor{",
+      i(1),
+      t"}{",
+      i(2),
+      t"}"
+    }
+  ),
+
+
+
 }
--- .config/nvim/luasnippets/tex/math.lua
+++ .config/nvim/luasnippets/tex/math.lua
@@ -1,5 +1,5 @@
 -- LaTeX math environment snippets (
-
+local utils = require('luasnippets.utils')
 local tex_utils = require('tex.utils')
 
 return {
@@ -15,38 +15,100 @@ return {
   s({trig="frak", dscr="fraktur typeface", snippetType="autosnippet"},
     fmta(
       [[
-      \mathfraktur{<>} <>
+      \mathfrak{<>} <>
       ]],
-    { i(1), i(0) }
-  ),
-  { condition = tex_utils.in_mathzone }
-),
-s({ trig = "bf", dscr = "blackboard typeface", snippetType="autosnippet"},
-  fmt(
-    [[
-    \mathbb{<>}
-    ]],
-    { i(1) },
-    { delimiters = "<>" }
-  ),
-  { condition = tex_utils.in_mathzone }  
-),
-s({ trig = "frac", dscr = "trigger fraction environment"},
-  fmta(
-    [[
-    \frac{<>}{<>}
-    ]],
-    { i(1,"num"), i(2,"den") }
+      { i(1), i(0) }
     ),
     { condition = tex_utils.in_mathzone }
   ),
-s({ trig = "boo", dscr = "test function", snippetType="autosnippet" },
-  fmta([[
+  s({ trig = "bf", dscr = "math bold typeface", snippetType="autosnippet" },
+    fmta(
+      [[
+    \mathbf{<>} <>
+    ]],
+      {
+        i(1), i(0)
+      }
+    ),
+    { condition = tex_utils.in_mathzone }
+  ),
+  s({ trig = "bb", dscr = "blackboard typeface", snippetType="autosnippet"},
+    fmt(
+      [[
+    \mathbb{<>}
+    ]],
+      { i(1) },
+      { delimiters = "<>" }
+    ),
+    { condition = tex_utils.in_mathzone }  
+  ),
+  s({ trig = "ff", dscr = "trigger fraction environment", snippetType="autosnippet"},
+    fmta(
+      [[
+    \frac{<>}{<>}
+    ]],
+      { i(1,"num"), i(2,"den") }
+    ),
+    { condition = tex_utils.in_mathzone }
+  ),
+  s({ trig = "boo", dscr = "test function", snippetType="autosnippet" },
+    fmta([[
     The result of tex_utils.in_mathzone is : <> 
     The result of tex_utils.in_text is : 
   ]],
-    {i(1)}
+      {
+        i(1)
+      }
     ),
     { condition=utils.in_mathzone}
-),
+  ),
+-- Superscript snippet
+  s({ trig = "([%w%)%]%}])'", dscr = "Superscript", wordTrig=false, regTrig=true,snippetType="autosnippet" },
+    fmta(
+    "<>^{<>}",
+      {
+        f( function(_, snip) return snip.captures[1] end ),
+        d(1, utils.get_visual),
+      }
+    ),
+    { condition = tex_utils.in_mathzone }
+  ),  
+-- Subscript snippet
+  s({trig = "([%w%)%]%}])_", wordTrig=false, regTrig = true, snippetType="autosnippet"},
+    fmta(
+      "<>_{<>}",
+      {
+        f( function(_, snip) return snip.captures[1] end ),
+        d(1, utils.get_visual),
+      }
+    ),
+    {condition = tex_utils.in_mathzone}
+  ),
+  s({trig = "([%w%)%]%}])\"", wordTrig=false, regTrig = true, snippetType="autosnippet"},
+    fmta(
+      "<>^{<>}_{<>}",
+      {
+        f( function(_, snip) return snip.captures[1] end ),
+        i(1),
+        i(2),
+      }
+    ),
+    {condition = tex_utils.in_mathzone}
+  ),
+  
+  ms({ 
+    common = {
+      snippetType="autosnippet", condition=tex_utils.in_mathzone
+    },
+    "cnr",
+    "binom",
+    "ncr"
+  }, {
+      t"\\binom{",
+      i(1),
+      t"}{",
+      i(2),
+      t"}",
+      i(0)
+  })
 }
