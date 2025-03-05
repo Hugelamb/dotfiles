53324566099f08e88524fd1cab9f18e3a14f8137
Hugh Lam <hughrjlam@gmail.com>
2025-03-05 11:36:47 +1100

    updated templates

--- .config/nvim/luasnippets/tex/templates.lua
+++ .config/nvim/luasnippets/tex/templates.lua
@@ -40,22 +40,36 @@ return {
     ),
     { condition = tex_utils.in_text + conds.line_begin }
   ),
-  s({ trig = "lecture", dscr = "Lecture template" },
+  -- The following assumes you have defined certain volleyball related lengths
+  s({ trig = "vballcourt", dscr = "create tikzpicture containing empty halfcourt"},
     fmta(
-    [[
-    \documentclass[12pt,a4paper]{article}
-
-    %packages
-    \usepackage{amsmath, amssymb, xcolor, enumerate}
-
-    \begin{document}
-      <>
-    \end{document}
-    ]],
+      [[
+      \begin{figure}
+        \pagecolor{<>}
+        \caption{<>}
+        \begin{tikzpicture}
+          \begin{scope}[yshift=10cm]
+            % define some useful coordinates
+            \coordinate (origin) at (0,0);
+            \coordinate (leftant) at (-4.5\m,0\m);
+            \coordinate(rightant) at (4.5\m,0\m);
+            \coordinate (attacklinecenter) at (0,-3\m);
+            \draw[fill=brown,draw=black] ([xshift=-.1\m,yshift=-.05\m]leftant) rectangle ++ (0.1\m,0.1\m);
+            \draw[fill=brown,draw=black] ([xshift=.1\m,yshift=-.05\m]rightant) rectangle ++ (-0.1\m,0.1\m);
+            \draw[draw=none] ([yshift=-\sidelength]leftant) rectangle (rightant); 
+            \draw[courtlines] ([yshift=0.5\courtlinewidth]leftant) rectangle ([yshift=-0.5\courtlinewidth]rightant);
+            \draw[courtlines] (leftant) rectangle ++ (\courtlinewidth,-\sidelength);
+            \draw[courtlines] (rightant) rectangle ++ (-\courtlinewidth,-\sidelength);
+            \draw[courtlines] ([yshift=-\sidelength]leftant) rectangle ++ (\baselength,\courtlinewidth);
+            \draw[courtlines] ([yshift=-\sidelength/3]leftant) rectangle ++ (\baselength,\courtlinewidth);
+            <>
+          \end{scope}
+        \end{tikzpicture}
+      \end{figure}
+      ]],
       {
-        i(0)
+        i(2),i(1),i(0)
       }
-    ),
-    { condition = tex_utils.in_text + conds.line_begin }
-  ),  
-}
+      )
+    ),  
+  }
